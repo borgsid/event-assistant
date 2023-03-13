383b89939d16e0c15de047dd313d73c50729ed5f
borgsid <s-i-l@hotmail.it>
2023-03-13 18:40:09 +0100

    drastic change

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -4,26 +4,44 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
+const configurationCurie = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY_Curie,
+});
 
+const openai = new OpenAIApi(configuration);
+const openaiCurie = new OpenAIApi(configurationCurie);
+const tg_dict={
+  'Furniture': 'Fu',
+  'Kitchen': 'Ki',
+  'Exteriors': 'Ex',
+  'Installations': 'In',
+  'Sustainability': 'Su',
+  'Art': 'Ar',
+  'Lighting': 'Li',
+  'Product Design': 'Pr',
+  'Architecture': 'Ar',
+  'Graphic Design': 'Gr',
+  'Party': 'Pa',
+  'Selected': 'Se',
+  'Mention': 'Me',
+  'Bathroom': 'Ba',
+  'Special materials': 'Sp',
+  'Technology': 'Te'
+}
 const generateAction = async (req, res) => {
-  const basePromptPrefix = `You are jill, a help desk agent for the 2021 fuorisalone, you only give answers related to fuorisalone, transportation, restaurants and hospitality in Milano and Lombardia Region. Always sign off your reply with a friendly emoji.
-  me: {userPromt}\n`.replace("{userPromt}", req.body.userInput);
-  const jillSection = `jill:`;
-  
-  const finalPromt=`${basePromptPrefix}${jillSection}`;
-  // Run first prompt
+  const basePromptPrefix = `:${req.body.userInput} ->`;
+  const curieRespose = await openaiCurie.createCompletion({
+    model: 'curie:ft-personal-2023-03-07-19-29-49',
+    prompt: `${basePromptPrefix} ->` ,
+    temperature: 0,
+    max_tokens: 1000,
+    top_p: 1,
+    frequency_penalty: 2,
+    presence_penalty: 1,
+  });
+  const finalPromt=`${basePromptPrefix}`;
   console.log(`API: ${finalPromt}`)
 
-  const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: finalPromt,
-    temperature: 0.7,
-    max_tokens: 250,
-  });
-
-  const basePromptOutput = baseCompletion.data.choices.pop();
-
-  res.status(200).json({ output: basePromptOutput });
+  res.status(200).json({ output: curieRespose.data.choices.pop() });
 };
 export default generateAction;
